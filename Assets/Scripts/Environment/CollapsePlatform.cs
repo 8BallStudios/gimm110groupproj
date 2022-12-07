e7b12ebce2fe587711f0b56898432261c6a52f08
jackiejua <jackiejuarez@u.boisestate.edu>
2022-12-06 19:58:17 -0700

    Added platform prefabs and part of floor

---
+++ Assets/Scripts/Environment/CollapsePlatform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//On CollapsingPlatfroms in main scene
+public class CollapsePlatform : MonoBehaviour
+{
+    //Variable game component for physics
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Starts off with the rb component 
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    //Brings in collision elements
+    void OnCollisionEnter2D (Collision2D col)
+    {   
+        //If player is the object to collide
+        if (col.gameObject.name.Equals("Player"))
+        {
+            //Allows function to occur
+            Invoke ("DropPlatform", 0.5f);
+            //Platform will disappear after drop
+            Destroy (gameObject, 2f);
+        }
+    }
+
+    //Platfrom collapses/drops
+    void DropPlatform()
+    {
+        rb.isKinematic = false;
+    }
+}
---
+++ Assets/Scripts/Environment/StickyPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//On moving platform in main scene
+public class StickyPlatform : MonoBehaviour
+{
+    //Platform become parent when player collides with it
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    //It doesn't when player gets off
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+}
